bd80d15ff565f7445002c3e90ae782b71dfaaa6a
Anna Pilch <s25630@pjwstk.edu.pl>
2024-04-28 18:31:17 +0200

    Initial commit

---
+++ APBD_Zadanie_6/Controllers/WarehouseProcedureController.cs
@@ -0,0 +1,16 @@
+﻿using APBD_Task_6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD_Zadanie_6.Controllers
+{
+    [Route("api/warehouse2")]
+    [ApiController]
+    public class WarehouseProcedureController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddProductToWarehouse(ProductWarehouse productWarehouse)
+        {
+            return View();
+        }
+    }
+}
